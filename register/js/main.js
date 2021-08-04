8cd48a7cd75e9452f3691e452d5d84bdafd67775
Malak Mamdouh <62308034+malakmamdouh4@users.noreply.github.com>
2021-08-04 16:25:22 +0200

    Add files via upload

---
+++ register/js/main.js
@@ -0,0 +1,38 @@
+
+
+
+!(function($) {
+    "use strict";
+  
+    // Mobile Navigation
+    if ($('.nav-menu').length) {
+      var $mobile_nav = $('.nav-menu').clone().prop({
+        class: 'mobile-nav d-lg-none'
+      });
+  
+     
+    } else if ($(".mobile-nav, .mobile-nav-toggle").length) {
+      $(".mobile-nav, .mobile-nav-toggle").hide();
+    }
+  
+  
+    // jQuery counterUp
+    $('[data-toggle="counter-up"]').counterUp({
+      delay: 10,
+      time: 1000
+    });
+  
+  })(jQuery);
+  
+  
+  // login form 
+  
+  var modal = document.getElementById('id01');
+  
+  window.onclick = function(event) {
+      if (event.target == modal) {
+          modal.style.display = "none";
+      }
+  }
+  
+  
